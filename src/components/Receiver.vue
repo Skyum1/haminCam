e77479cc84d3c5bd01d47b929e3cd049a793be36
Skyum <rjftkd126@gmail.com>
2026-06-07 19:43:41 +0900

    변경

--- src/components/Receiver.vue
+++ src/components/Receiver.vue
@@ -22,18 +22,11 @@ import { io } from 'socket.io-client'
 const remoteVideo = ref(null)
 
 // const socket = io('http://192.168.45.126:3000')
-let socket;
+let socket = window.location.hostname.startsWith('192.168.') ? io('https://192.168.45.95:3000') : io('https://hamincam.mooo.com:3000')
 // 본인 PC IP로 변경
 
 let pc = null
 
-onMounted(() => {
-  if(window.location.hostname.startsWith('192.168.')) {
-    socket = io('https://hamincam.mooo.com:3000')
-  } else {
-    socket = io('https://192.168.45.95:3000')
-  }
-})
 const peerConnection = new RTCPeerConnection({
   iceServers: [
     {
